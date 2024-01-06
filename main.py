a2906e321d56080990f3728b7ba74321f883bb25
Abid Hasan <144882936+abid450@users.noreply.github.com>
2024-01-06 20:36:22 +0600

    I have a upload python file

---
+++ main.py
@@ -0,0 +1,292 @@
+# Vote Election Project _------------_-------------_--_------
+# Example For Vote Person Details :---------
+"""Person 1 = Abid,Vote No :  2900
+   Person 2 = Sayem,Vote No : 3400
+   Person 3 = Jihad,Vote No : 4400
+   Person 4 = Raisha,Vote No :5400
+   Person 5 = Sazzad,Vote No :6400
+   Person 6 = Karim,Vote No : 7400
+   Person 7 = Tutul,Vote No : 8400
+   Person 8 = Jihan,Vote No : 9400
+   Person 9 = Maruf,Vote No :  108
+   Person 10 = Akash,Vote No : 201
+   Person 11 = Totini,Vote No :301
+   Person 12 = Mahin,Vote No : 503
+   Person 13 = Yash,Vote No   : 701
+   Person 14 = Faria,Vote No :  609
+"""
+
+# Person 1
+Name = input("Your Name    : ")
+if Name == "Abid":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Abid":
+    
+    if nid1 == "2900":
+        print("Vote Submited/")
+        print("Now Submit 2 No Vote............")
+    else:
+        print("Did Not Submit")
+        print("Now Submit 2 No Vote............")
+else:
+   print("Did Not Submit")
+   print("Now Submit 2 No Vote............")
+
+# Person 2
+Name = input("Your Name    : ")
+if Name == "Sayem":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Sayem":
+    
+    if nid1 == "3400":
+        print("Vote Submited/")
+        print("Now Submit 3 No Vote............")
+    else:
+        print("Did Not Submit")
+        print("Now Submit 3 No Vote............")
+else:
+   print("Did Not Submit")
+   print("Now Submit 3 No Vote............")
+
+# Person 3----------
+Name = input("Your Name    : ")
+if Name == "Jihad":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Jihad":
+    
+    if nid1 == "4400":
+        print("Vote Submited/")
+        print("Now Submit 4 No Vote............")   
+    else:
+        print("Did Not Submit")
+        print("Now Submit 4 No Vote............")
+else:
+   print("Did Not Submit")
+   print("Now Submit 4 No Vote............")
+
+# Person 4
+Name = input("Your Name    : ")
+if Name == "Raisha":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Raisha":
+    
+    if nid1 == "5400":
+        print("Vote Submited/")
+        print("Now Submit 5 No Vote............")
+    else:
+        print("Did Not Submit")
+        print("Now Submit 5 No Vote............")
+else:
+   print("Did Not Submit")
+   print("Now Submit 5 No Vote............")
+
+# Person 5--------------
+Name = input("Your Name    : ")
+if Name == "Sazzad":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Sazzad":
+    
+    if nid1 == "6400":
+        print("Vote Submited/")
+        print("Now Submit 6 No Vote............")
+    else:
+        print("Did Not Submit")
+        print("Now Submit 6 No Vote............")
+else:
+   print("Did Not Submit")
+   print("Now Submit 6 No Vote............")
+
+# Person 6
+Name = input("Your Name    : ")
+if Name == "Karim":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Karim":
+    
+    if nid1 == "7400":
+        print("Vote Submited/")
+        print("Now Submit 7 No Vote............")
+    else:
+        print("Did Not Submit")
+        print("Now Submit 7 No Vote............")
+else:
+   print("Did Not Submit")
+   print("Now Submit 7 No Vote............")
+
+# Person 7 -----------
+Name = input("Your Name    : ")
+if Name == "Tutul":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Tutul":
+    
+    if nid1 == "8400":
+        print("Vote Submited/")
+        print("Now Submit 8 No Vote............")
+    else:
+        print("Did Not Submit")
+        print("Now Submit 8 No Vote............")
+else:
+   print("Did Not Submit")
+   print("Now Submit 8 No Vote............")
+
+# Person 8 -----------
+Name = input("Your Name    : ")
+if Name == "Jihan":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Jihan":
+    
+    if nid1 == "9400":
+        print("Vote Submited/")
+        print("Now Submit 9 No Vote............")
+    else:
+        print("Did Not Submit")
+        print("Now Submit 9 No Vote............")
+else:
+   print("Did Not Submit")
+   print("Now Submit 9 No Vote............")
+
+# Person 9 -----------
+Name = input("Your Name    : ")
+if Name == "Maruf":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Maruf":
+    
+    if nid1 == "108":
+        print("Vote Submited/")
+        print("Now Submit 10 No Vote............")
+    else:
+        print("Did Not Submit")
+        print("Now Submit 10 No Vote............")
+else:
+   print("Did Not Submit")
+   print("Now Submit 10 No Vote............")
+
+# Person 10 -----------
+Name = input("Your Name    : ")
+if Name == "Akash":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Akash":
+    
+    if nid1 == "201":
+        print("Vote Submited/")
+        print("Now Submit 11 No Vote............")
+    else:
+        print("Did Not Submit")
+        print("Now Submit 11 No Vote............")
+else:
+   print("Did Not Submit")
+   print("Now Submit 11 No Vote............")
+
+# Person 11 -----------
+Name = input("Your Name    : ")
+if Name == "Totini":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Totini":
+    
+    if nid1 == "301":
+        print("Vote Submited/")
+        print("Now Submit 12 No Vote............")
+    else:
+        print("Did Not Submit")
+        print("Now Submit 12 No Vote............")
+else:
+   print("Did Not Submit")
+   print("Now Submit 12 No Vote............")
+
+# Person 12 -----------
+Name = input("Your Name    : ")
+if Name == "Mahin":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Mahin":
+    
+    if nid1 == "503":
+        print("Vote Submited/")
+        print("Now Submit 13 No Vote............")
+    else:
+        print("Did Not Submit")
+        print("Now Submit 13 No Vote............")
+else:
+   print("Did Not Submit")
+   print("Now Submit 13 No Vote............")
+
+# Person 13 -----------
+Name = input("Your Name    : ")
+if Name == "Yash":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Yash":
+    
+    if nid1 == "701":
+        print("Vote Submited/")
+        print("Now Submit 14 No Vote............")
+    else:
+        print("Did Not Submit")
+        print("Now Submit 14 No Vote............")
+        
+else:
+   print("Did Not Submit")
+   print("Now Submit 14 No Vote............")
+
+# Person 14 -----------
+Name = input("Your Name    : ")
+if Name == "Faria":
+    print("Please Submit The Vote Number")
+else:
+    print("Please Enter The Correct Name")
+nid1 = input("Enter Your Vote Number  : ")
+if Name == "Faria":
+    
+    if nid1 == "609":
+        print("Vote Submited/")
+        print("Complete The Vote Number.....")
+        print("The Proggram Ends -------")
+    
+        
+else:
+   print("Did Not Submit")
+   print("Complete The Vote Number.....")
+   print("The Proggram Ends -------")
+
+# Ends -------------------------------------------------
+
+
+
+
+   
